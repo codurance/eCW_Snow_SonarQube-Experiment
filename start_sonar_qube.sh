7b26d5996a02469009f352ed746ad9ddf5865dc7
Florian Kempenich <Flori@nKempenich.com>
2022-10-12 15:34:35 +0100

    Use M1 compatible img if needed

--- start_sonar_qube.sh
+++ start_sonar_qube.sh
@@ -6,19 +6,18 @@ cd "$DIR"
 
 
 
-
+if [[ $(arch) == 'arm64' ]]; then
+    IMG='mwizner/sonarqube:8.9.5-community'
+else
+    IMG='sonarqube:lts'
+fi
 
 docker run -d \
-    --platform linux/amd64 \
     --rm \
     --name sonarqube \
     -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true \
     -p 9000:9000 \
-    sonarqube:lts
-
-
-
-
+    $IMG
 
 
 
